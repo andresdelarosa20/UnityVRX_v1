9f0b8747df78d0631caf96fbdd5050dc5d3d071e
Andrés De la Rosa <andres.delarosa340@gmail.com>
2026-05-04 20:00:25 -0500

    Animación Puertas

---
+++ UnityVRX_v1/Assets/Scripts/PuertaControlador.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaControlador : MonoBehaviour
+{
+    public Animator doorAnimator;
+
+    //abrir puerta
+    [ContextMenu("Abrir Puerta")]
+    public void abrirPuerta()
+    {
+        print("puerta abierta");
+        doorAnimator.SetBool("Abierta", true);
+    }
+
+    //cerrar puerta
+    [ContextMenu("Cerrar Puerta")]
+    public void cerrarPuerta()
+    {
+        print("puerta cerrada");
+        doorAnimator.SetBool("Abierta", false);
+
+    }
+
+    //puerta bloqueada
+    [ContextMenu("Bloquear Puerta")]
+    public void puertaBloqueada()
+    {
+        print("puerta bloqueada");
+    }
+}
